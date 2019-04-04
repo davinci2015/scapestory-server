c8d4bb3fbde8d0aa4e2b8f5f3f1e08a5d760dff8
davinci2015 <danijel.vincijanovic@gmail.com>
2019-04-04 13:53:38 +0200

    Added next setup to the client folder

---
+++ client/pages/index.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import Link from 'next/link'
+
+const Index = () => (
+	<React.Fragment>
+		<h1>hello</h1>
+		<Link href="/about">About</Link>
+	</React.Fragment>
+)
+
+export default Index
