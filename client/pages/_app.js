6b44f727c3d5b7018d74df5809c5d039e3bb376a
davinci2015 <danijel.vincijanovic@gmail.com>
2019-05-09 19:51:35 +0200

    Updated models | Added ApolloProvider

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import App, { Container } from 'next/app'
+import { ApolloProvider } from 'react-apollo'
+import { apolloClient } from '../graphql/apollo'
 
 export default class MyApp extends App {
 	render() {
 		const { Component, pageProps } = this.props
 		return (
 			<Container>
-				<Component {...pageProps} />
+				<ApolloProvider client={apolloClient}>
+					<Component {...pageProps} />
+				</ApolloProvider>
 			</Container>
 		)
 	}
