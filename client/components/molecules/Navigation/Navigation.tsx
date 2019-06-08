a56f6719cc27af1a6d8e256791a6a922b92e1d5d
davinci2015 <danijel.vincijanovic@gmail.com>
2019-06-08 22:29:08 +0200

    Updated navigation styles

--- client/components/molecules/Navigation/Navigation.tsx
+++ client/components/molecules/Navigation/Navigation.tsx
@@ -1,27 +1,8 @@
 import React from 'react'
-import css from 'styled-jsx/css'
 import Link from 'next/link'
 import routes, {routeMapping} from 'routes'
 import * as styles from 'styles'
 import AuthorizationGuard from 'components/atoms/AuthorizationGuard'
-
-const style = css`
-    nav {
-        width: 100%;
-        height: 64px;
-        background-color: ${styles.colors.TERTIARY};
-        box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
-    }
-    
-    .container {
-        max-width: 70%;
-        height: 100%;
-        display: flex;
-        margin: 0 auto;
-        align-items: center;
-        justify-content: space-between;
-    }
-`
 
 const Navigation = () => (
     <nav>
@@ -41,7 +22,25 @@ const Navigation = () => (
                 <AuthorizationGuard>Guard</AuthorizationGuard>
             </div>
         </div>
-        <style jsx>{style}</style>
+
+        <style jsx>{`
+            nav {
+                width: 100%;
+                height: 64px;
+                background-color: ${styles.colors.TERTIARY};
+                box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+            }
+            
+            .container {
+                max-width: 70%;
+                height: 100%;
+                display: flex;
+                margin: 0 auto;
+                align-items: center;
+                justify-content: space-between;
+            }
+        `}</style>
+
     </nav>
 )
 
