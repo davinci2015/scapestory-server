ec0cf06a2426448f328cbab57dc2c6b4591fd19e
davinci2015 <davinci02014@gmail.com>
2020-01-15 23:20:49 +0100

    Select styles

--- client/components/atoms/SelectCreatable/SelectCreatable.tsx
+++ client/components/atoms/SelectCreatable/SelectCreatable.tsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, {CSSProperties} from 'react'
 import CreatableSelect from 'react-select/creatable'
-import {ActionMeta, ValueType, InputActionMeta, GroupedOptionsType, OptionsType} from 'react-select'
+import {
+    ActionMeta,
+    ValueType,
+    InputActionMeta,
+    GroupedOptionsType,
+    OptionsType,
+    Theme,
+} from 'react-select'
+
+import {colors} from 'styles'
 
 export interface Props<OptionType> {
     options: GroupedOptionsType<OptionType> | OptionsType<OptionType>
@@ -10,6 +19,26 @@ export interface Props<OptionType> {
     filterOptions?: (option: {}, inputValue: string) => boolean
     placeholder?: React.ReactNode
 }
+
+const styles = {
+    option: (styles: CSSProperties, {isFocused}: any) => ({
+        ...styles,
+        backgroundColor: isFocused ? colors.PRIMARY_LIGHT : colors.WHITE,
+    }),
+}
+const theme = (theme: Theme) => ({
+    ...theme,
+    colors: {
+        ...theme.colors,
+        primary25: colors.PRIMARY_LIGHT,
+        primary: colors.PRIMARY,
+        danger: colors.ERROR,
+        neutral0: colors.WHITE,
+        neutral10: colors.SHADE_EXTRA_LIGHT,
+        neutral50: colors.MID_GRAY,
+        neutral90: colors.DARK_GRAY,
+    },
+})
 
 const SelectCreatable = function<OptionType>({
     filterOptions,
@@ -25,6 +54,8 @@ const SelectCreatable = function<OptionType>({
             onCreateOption={onCreateOption}
             filterOption={filterOptions}
             placeholder={placeholder}
+            styles={styles}
+            theme={theme}
         />
     )
 }
