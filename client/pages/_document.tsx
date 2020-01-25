ec49b1b88a601540d730ac46de3d4f00178b6b07
davinci2015 <davinci02014@gmail.com>
2020-01-25 13:39:27 +0100

    Added react-slick as dependency

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -20,6 +20,16 @@ class MyDocument extends Document {
                         href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,600,800,900&display=swap&subset=latin-ext"
                         rel="stylesheet"
                     />
+                    <link
+                        rel="stylesheet"
+                        type="text/css"
+                        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+                    />
+                    <link
+                        rel="stylesheet"
+                        type="text/css"
+                        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+                    />
                     <link href="/static/ReactToastify.min.css" rel="stylesheet" />
                     {flush()}
                 </Head>
