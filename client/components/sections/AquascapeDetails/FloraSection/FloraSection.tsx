6c85be79d806427fed094a16d77f597048ad94bb
davinci2015 <davinci02014@gmail.com>
2020-01-27 13:25:56 +0100

    FloraSection responsive

--- client/components/sections/AquascapeDetails/FloraSection/FloraSection.tsx
+++ client/components/sections/AquascapeDetails/FloraSection/FloraSection.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import {FormattedMessage, Headline} from 'components/atoms'
 import {List} from 'components/molecules'
-import {spaces} from 'styles'
+import {spaces, media} from 'styles'
 
 const FloraSection: React.FunctionComponent = ({children}) => (
     <>
@@ -22,11 +22,24 @@ const FloraSection: React.FunctionComponent = ({children}) => (
 
             .list {
                 display: flex;
-                margin-top: ${spaces.s90};
+                flex-direction: column;
+                margin-top: ${spaces.s60};
             }
 
             .list :global(.${List.classes.root}) {
+                margin-top: ${spaces.s36};
                 flex: 1 1 0px;
+            }
+
+            @media ${media.up('medium')} {
+                .list {
+                    flex-direction: row;
+                    margin-top: ${spaces.s90};
+                }
+
+                .list :global(.${List.classes.root}) {
+                    margin-top: 0;
+                }
             }
         `}</style>
     </>
