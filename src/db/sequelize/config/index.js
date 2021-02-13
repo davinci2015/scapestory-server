473a3e2a8619f06656eef4f1b0de9afca6e7e1fa
davinci2015 <danijel.vincijanovic@gmail.com>
2021-02-14 00:08:08 +0100

    Updated config for sequelize

--- src/db/sequelize/config/index.js
+++ src/db/sequelize/config/index.js
@@ -6,10 +6,18 @@ require('dotenv').config({path: path.resolve(__dirname, '../../../../.env')})
 module.exports = {
     development: {
         url: process.env.DATABASE_URL,
+        ssl: true,
         dialect: 'postgres',
+        dialectOptions: {
+            ssl: true,
+        },
     },
     production: {
         url: process.env.DATABASE_URL,
+        ssl: true,
         dialect: 'postgres',
+        dialectOptions: {
+            ssl: true,
+        },
     },
 }
