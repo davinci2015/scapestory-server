ae64c4bbc34c23dcb2ee9ad0a5fdd482c1960f6d
davinci2015 <danijel.vincijanovic@gmail.com>
2021-02-14 10:08:01 +0100

    Updated sequelize connection

--- src/db/adapters/SequelizeAdapter.ts
+++ src/db/adapters/SequelizeAdapter.ts
@@ -47,12 +47,6 @@ export class SequelizeAdapter implements DatabaseAdapter {
 
     connect(params: DatabaseConnectionParams) {
         this.instance = new Sequelize(params.uri, {
-            dialectOptions: {
-                ssl: {
-                    require: true,
-                    rejectUnauthorized: false,
-                },
-            },
             models: [
                 Additive,
                 Aquascape,
--- src/db/sequelize/config/index.js
+++ src/db/sequelize/config/index.js
@@ -6,20 +6,22 @@ require('dotenv').config({path: path.resolve(__dirname, '../../../../.env')})
 module.exports = {
     development: {
         url: process.env.DATABASE_URL,
-        ssl: true,
         dialect: 'postgres',
         dialectOptions: {
-            ssl: true,
-            rejectUnauthorized: false,
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
         },
     },
     production: {
         url: process.env.DATABASE_URL,
-        ssl: true,
         dialect: 'postgres',
         dialectOptions: {
-            ssl: true,
-            rejectUnauthorized: false,
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
         },
     },
 }
